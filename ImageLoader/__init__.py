28d4b8adbaeccdd5bb80cc87becf2ecea698738f
Tim Streicher <streicher.tim@googlemail.com>
2018-05-19 11:14:29 +0200

    added image loader

---
+++ ImageLoader/__init__.py
@@ -0,0 +1,31 @@
+import numpy as np
+import scipy.ndimage as ndimage
+from enum import Enum
+import os
+import os.path
+
+
+pathToImages = "../images/"
+
+class Modus(Enum):
+    TEST = ""
+    TRAINING = "/Graffiti_mit_ohne"
+
+def loadTestImages():
+    images = []
+    testPath = pathToImages + Modus.TRAINING.value
+    for dirpath, dirnames, filenames in os.walk(testPath):
+        for dir in dirnames:
+            if "Training" in dir:
+                images.append(getImages(testPath+"/"+dir))
+    return images
+
+
+def getImages(path):
+    images = []
+    for _,_, files in os.walk(path):
+        for file in files:
+            images.append(ndimage.imread(path+"/"+file))
+    return images
+
+print(loadTestImages()     )
