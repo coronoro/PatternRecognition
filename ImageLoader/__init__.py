1254d951202dc8febfcb3f5f00b8695ba8e87915
Tim Streicher <streicher.tim@googlemail.com>
2018-05-19 12:45:15 +0200

    corrected return type of Imageloader

--- ImageLoader/__init__.py
+++ ImageLoader/__init__.py
@@ -4,7 +4,7 @@ from enum import Enum
 import os
 import os.path
 
-
+limit = 3
 
 pathToImages = "../images/Graffiti_mit_ohne"
 
@@ -12,27 +12,38 @@ class Modus(Enum):
     TEST = "Test"
     TRAINING =  "Training"
 
-def loadTestImages():
+def loadTrainingImages():
+    return loadImage(Modus.TRAINING)
+
+
+def loadImage(modus:Modus):
     allImages = []
     allDetect = []
     testPath = pathToImages
     for dirpath, dirnames, filenames in os.walk(testPath):
         for dir in dirnames:
-            if Modus.TRAINING.value in dir:
-                images, detect = getImages(testPath+"/"+dir)
-                allImages.append(images)
-                allDetect.append(detect)
-    return allImages, allDetect
+            if modus.value in dir:
+                images, detect = getImages(testPath,dir)
+                allImages.extend(images)
+                allDetect.extend(detect)
+    return np.array(allImages), np.array(allDetect)
 
+def loadTestImages():
+    return loadImage(Modus.TEST)
 
-def getImages(path):
+def getImages(path, dir):
     images = []
     detect = []
-    for dirpath ,_, files in os.walk(path):
+    temp = 0
+    for dirpath ,_, files in os.walk(path+ "/" + dir):
         for file in files:
-            images.append(ndimage.imread(path + "/" + file))
-            if "ohne" in dirpath:
-                detect.append(False)
+            if temp < limit:
+                images.append(ndimage.imread(path + "/" + dir+"/" + file))
+                if "ohne" in dir:
+                    detect.append(False)
+                else:
+                    detect.append(True)
+                temp+=1
             else:
-                detect.append(True)
+                break
     return images, detect
--- mnist/mnist.py
+++ mnist/mnist.py
@@ -9,15 +9,20 @@ from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
+import ImageLoader
+
+
 K.set_image_dim_ordering('th')
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 # load data
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+#(X_train, y_train), (X_test, y_test) = mnist.load_data()
+(X_train, y_train) = ImageLoader.loadTrainingImages()
+(X_test, y_test) = ImageLoader.loadTestImages()
 # reshape to be [samples][pixels][width][height]
-X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+X_train = X_train.reshape(X_train.shape[0], 3, 1200, 1600).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 3, 1200, 1600).astype('float32')
 # normalize inputs from 0-255 to 0-1
 X_train = X_train / 255
 X_test = X_test / 255
@@ -31,7 +36,7 @@ num_classes = y_test.shape[1]
 def larger_model():
 	# create model
 	model = Sequential()
-	model.add(Conv2D(30, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+	model.add(Conv2D(30, (5, 5), input_shape=(3, 1200, 1600), activation='relu'))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
 	model.add(Conv2D(15, (3, 3), activation='relu'))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
